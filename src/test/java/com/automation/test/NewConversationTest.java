90b1669c13251e9b60c8d42b12a59ab566e0eb2b
Alok <alokbharati001@gmail.com>
2026-06-29 09:23:32 +0530

    Add New Conversation page class for database

--- src/test/java/com/automation/test/NewConversationTest.java
+++ src/test/java/com/automation/test/NewConversationTest.java
@@ -1,4 +1,36 @@
 package com.automation.test;
 
-public class NewConversationTest {
+import com.automation.base.BaseTest;
+import com.automation.pages.NewConversionPage;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class NewConversationTest extends BaseTest {
+    NewConversionPage newConversionPage;
+    @BeforeClass
+    public void setUpPage() {
+        newConversionPage = new NewConversionPage(driver);  // driver comes from BaseTest
+    }
+    @Test(
+            groups = "newConversion",
+            dependsOnGroups="datasource"
+    )
+    public void newConversion() throws InterruptedException {
+        int maxAttempt=Integer.parseInt(properties.getProperty("maxAttempts"));
+        Thread.sleep(20000);
+        for(int i = 0; i < maxAttempt; i++) {
+            String statusMessage=newConversionPage.getDataSourceStatus();
+            if(statusMessage.equalsIgnoreCase(properties.getProperty("data_Source_Status"))){
+                newConversionPage.clickAskConversion();
+                Thread.sleep(10000);
+                newConversionPage.AskConversion(properties.getProperty("askQuestion"));
+                Thread.sleep(10000);
+                return;
+            }else {
+                Thread.sleep(5000);
+                newConversionPage.clickRefreshButton();
+            }
+        }
+        throw new RuntimeException("Data Source status did not become Completed.");
+    }
 }
